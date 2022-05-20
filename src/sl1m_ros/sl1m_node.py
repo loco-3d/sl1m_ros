a3e780eecad8d8a2d65760dbf00cd6c37ee3e96c
Maximilien Naveau <maximilien.naveau@gmail.com>
2022-05-20 17:26:10 +0200

    fix the parameters and rviz feedback

--- src/sl1m_ros/sl1m_node.py
+++ src/sl1m_ros/sl1m_node.py
@@ -1,18 +1,26 @@
-import rospy
+# standard imports
+import colorsys
 import threading
-from visualization_msgs.msg import MarkerArray
-from tf2_msgs.msg import TFMessage
-
 import time
 from copy import deepcopy
 import numpy as np
-import matplotlib.pyplot as plt
-from time import perf_counter as clock
 
+# imports from ROS
+import rospy
+from std_msgs.msg import Header
+from visualization_msgs.msg import MarkerArray, Marker
+from geometry_msgs.msg import Point
+from tf2_msgs.msg import TFMessage
+
+# local debug information
+import matplotlib.pyplot as plt
+plt.ion()
+from time import perf_counter as clock
+import sl1m.tools.plot_tools as sl1m_plot
+
+# local imports.
 from sl1m.generic_solver import solve_L1_combinatorial, solve_MIP
 from sl1m.problem_definition import Problem
-import sl1m.tools.plot_tools as plot
-
 from sl1m_ros.sl1m_params import Sl1mParameters
 
 
@@ -25,16 +33,27 @@ class Sl1mNode:
         # Handle the update of the input
         self.initial_contacts_mutex = threading.Lock()
         self.initial_contacts = [np.zeros(3), np.zeros(3)]
+        self.initial_contacts_frame_id = ""
         self.all_polygons_mutex = threading.Lock()
         self.all_polygons = []
+        self.all_polygons_frame_id = ""
 
         self.polygon_client = rospy.Subscriber(
-            "/sl1m_wrapper_node/polygons", MarkerArray, self.polygon_callback
+            "/sl1m_ros/polygons",
+            MarkerArray,
+            self.polygon_callback,
+            queue_size=5,
         )
         self.current_contact = rospy.Subscriber(
-            "/sl1m_wrapper_node/current_contact",
+            "/sl1m_ros/current_contact",
             TFMessage,
             self.current_contact_callback,
+            queue_size=5,
+        )
+        self.result_publisher = rospy.Publisher(
+            "/sl1m_ros/solution",
+            MarkerArray,
+            queue_size=5,
         )
 
         # This is valid because we loaded the parameter upon starting the node.
@@ -45,27 +64,48 @@ class Sl1mNode:
             suffix_feet=self.params.suffix_feet,
         )
 
+        # Colors
+        self.colors = [
+            (31, 119, 180),
+            (255, 127, 14),
+            (44, 160, 44),
+            (214, 39, 40),
+            (227, 119, 194),
+            (127, 127, 127),
+            (188, 189, 34),
+            (23, 190, 207),
+        ]
+
+        if self.params.plot:
+            self.fig = plt.figure("sl1m_ros results")
+            self.ax = self.fig.add_subplot(111, projection="3d")
+
     def current_contact_callback(self, msg):
-        rospy.loginfo(
-            "New contact list. Number of contact : {}".format(
-                len(msg.transforms)
-            )
-        )
+        # rospy.loginfo(
+        #     "current_contact_callback(): New contact list. Number of contact : {}".format(
+        #         len(msg.transforms)
+        #     )
+        # )
         assert len(self.params.limbs) == len(msg.transforms)
         self.initial_contacts_mutex.acquire()
         for i in range(len(self.initial_contacts)):
-            self.initial_contacts[i] = [
+            self.initial_contacts[i][:] = [
                 msg.transforms[i].transform.translation.x,
                 msg.transforms[i].transform.translation.y,
                 msg.transforms[i].transform.translation.z,
             ]
-        rospy.loginfo("Current contacts: {}".format(self.initial_contacts))
+        # rospy.loginfo(
+        #     "current_contact_callback(): {}".format(self.initial_contacts)
+        # )
+        self.initial_contacts_frame_id = msg.transforms[0].header.frame_id
         self.initial_contacts_mutex.release()
 
     def polygon_callback(self, msg):
-        rospy.loginfo(
-            "New polygon list. Number of surfaces : {}".format(len(msg.markers))
-        )
+        # rospy.loginfo(
+        #     "polygon_callback(): New polygon list. Number of surfaces : {}".format(
+        #         len(msg.markers)
+        #     )
+        # )
 
         # tmp var
         all_polygons = []
@@ -76,47 +116,43 @@ class Sl1mNode:
             for point in surface.points:
                 point_list.append([point.x, point.y, point.z])
             all_polygons.append(np.array(point_list).T)
+            self.all_polygons_frame_id = surface.header.frame_id
 
         self.all_polygons_mutex.acquire()
-        self.all_polygons = all_polygons
+        self.all_polygons = deepcopy(all_polygons)
         self.all_polygons_mutex.release()
 
-    def plot_if_results(self):
+    def plot_if_results(self, surfaces, initial_contacts, result):
         if self.params.plot:
-            ax = plot.draw_scene(self.surfaces, self.params.gait)
-            plot.plot_initial_contacts(self.initial_contacts, ax=ax)
-            if self.result.success:
-                plot.plot_planner_result(
-                    self.result.all_feet_pos,
-                    coms=self.result.coms,
-                    ax=ax,
-                    show=True,
+            self.ax = sl1m_plot.draw_scene(surfaces, self.params.gait, ax=self.ax)
+            sl1m_plot.plot_initial_contacts(initial_contacts, ax=self.ax)
+            if result.success:
+                sl1m_plot.plot_planner_result(
+                    result.all_feet_pos,
+                    coms=result.coms,
+                    ax=self.ax,
+                    show=False,
                 )
-            else:
-                plt.show()
+            plt.draw()
+            plt.pause(0.001)
 
     def run(self):
-        all_polygons = deepcopy(self.all_polygons)
-        initial_contacts = deepcopy(self.initial_contacts)
-
-        r = rospy.Rate(self.params.rate)
+        no_valid_input = True
+        ros_rate = rospy.Rate(self.params.rate)
         while not rospy.is_shutdown():
             # Get the time at the beginning of the loop
             t_start = clock()
-
-            rospy.loginfo("Wait for valid inputs...")
-            no_valid_input = True
             while no_valid_input:
                 # Work with local copies
-                self.initial_contacts_mutex.acquire()
+                self.all_polygons_mutex.acquire()
                 all_polygons = deepcopy(self.all_polygons)
-                self.initial_contacts_mutex.release()
+                self.all_polygons_mutex.release()
 
                 self.initial_contacts_mutex.acquire()
                 initial_contacts = deepcopy(self.initial_contacts)
                 self.initial_contacts_mutex.release()
 
-                if all_polygons == [] and np.all(
+                if not all_polygons or np.all(
                     np.array(initial_contacts)
                     == np.array([np.zeros(3), np.zeros(3)])
                 ):
@@ -124,9 +160,16 @@ class Sl1mNode:
                 else:
                     no_valid_input = False
 
-            rospy.loginfo("Input acquired.")
-            rospy.loginfo("all_polygons: {}".format(all_polygons))
-            rospy.loginfo("initial_contacts: {}".format(initial_contacts))
+            # Work with local copies
+            self.all_polygons_mutex.acquire()
+            all_polygons = deepcopy(self.all_polygons)
+            self.all_polygons_mutex.release()
+
+            self.initial_contacts_mutex.acquire()
+            initial_contacts = deepcopy(self.initial_contacts)
+            self.initial_contacts_mutex.release()
+
+            self.add_start_polygon(all_polygons, initial_contacts)
 
             t_acquiring_data = clock()
 
@@ -142,54 +185,210 @@ class Sl1mNode:
 
             t_problem = clock()
 
-            if self.params.solver_method == "L1":
+            if self.params.use_sl1m:
                 result = solve_L1_combinatorial(
                     self.problem,
-                    costs=self.params.cost,
+                    costs=self.params.costs,
                     com=self.params.optimize_com,
-                    solver=self.params.get_solver_type(),
+                    lp_solver=self.params.get_solver_type(),
+                    qp_solver=self.params.get_solver_type()
                 )
             else:
                 result = solve_MIP(
                     self.problem,
-                    costs=self.params.cost,
+                    costs=self.params.costs,
                     com=self.params.optimize_com,
                     solver=self.params.get_solver_type(),
                 )
 
             t_end = clock()
 
-            rospy.loginfo(
-                "Optimized number of steps: {}".format(self.problem.n_phases)
-            )
-            rospy.loginfo(
-                "Total time is: {}".format(1000.0 * (t_end - t_start))
-            )
-            rospy.loginfo(
-                "Acquiring the data takes: {}".format(
-                    1000.0 * (t_acquiring_data - t_start)
+            if not result.success:
+                rospy.loginfo("Result.succeded: {}".format(result.success))
+                rospy.loginfo("Result.time: {}".format(result.time))
+                rospy.loginfo(
+                    "Optimized number of steps: {}".format(self.problem.n_phases)
                 )
-            )
-            rospy.loginfo(
-                "Computing the surfaces takes : {}".format(
-                    1000.0 * (t_surface - t_acquiring_data)
+                rospy.loginfo(
+                    "Total time is: {}".format(1000.0 * (t_end - t_start))
                 )
-            )
-            rospy.loginfo(
-                "Generating the problem dictionary takes: {}".format(
-                    1000.0 * (t_problem - t_surface)
+                rospy.loginfo(
+                    "Acquiring the data takes: {}".format(
+                        1000.0 * (t_acquiring_data - t_start)
+                    )
                 )
-            )
-            rospy.loginfo(
-                "Solving the problem takes: {}".format(
-                    1000.0 * (t_end - t_problem)
+                rospy.loginfo(
+                    "Computing the surfaces takes : {}".format(
+                        1000.0 * (t_surface - t_acquiring_data)
+                    )
                 )
-            )
-            rospy.loginfo(
-                "The LP and QP optimizations take: {}".format(result.time)
-            )
-            self.surfaces = surfaces
-            self.result = result
-            self.initial_contacts = initial_contacts
-            self.plot_if_results()
-            r.sleep()
+                rospy.loginfo(
+                    "Generating the problem dictionary takes: {}".format(
+                        1000.0 * (t_problem - t_surface)
+                    )
+                )
+                rospy.loginfo(
+                    "Solving the problem takes: {}".format(
+                        1000.0 * (t_end - t_problem)
+                    )
+                )
+                rospy.loginfo(
+                    "The LP and QP optimizations take: {}".format(result.time)
+                )
+                rospy.loginfo("result.coms = {}".format(result.coms))
+                rospy.loginfo(
+                    "result.moving_feet_pos = {}".format(result.moving_feet_pos)
+                )
+                rospy.loginfo(
+                    "result.all_feet_pos = {}".format(result.all_feet_pos)
+                )
+                rospy.loginfo(
+                    "result.surface_indices = {}".format(result.surface_indices)
+                )
+
+            self.plot_if_results(all_polygons, initial_contacts, result)
+
+            result_ros = self.result_to_ros_msg(result, all_polygons)
+            self.result_publisher.publish(result_ros)
+
+            ros_rate.sleep()
+
+    def result_to_ros_msg(self, result, polygons):
+        ret = MarkerArray()
+        commun_header = Header()
+        coms = Marker()
+        if result.coms:
+            for pose in result.coms:
+                p = Point()
+                p.x = pose[0]
+                p.y = pose[1]
+                p.z = pose[2]
+                coms.points.append(p)
+            r, g, b = self.colors[len(result.all_feet_pos) + 1]
+            coms.color.r = r
+            coms.color.g = g
+            coms.color.b = b
+            coms.header = commun_header
+            coms.header.frame_id = self.all_polygons_frame_id
+            coms.type = Marker.LINE_STRIP
+            coms.action = Marker.ADD
+            coms.id = 0
+            coms.pose.orientation.w = 1.0
+            coms.scale.x = 0.05
+            coms.scale.y = 0.05
+            coms.scale.z = 0.05
+            coms.color.a = 1.0
+            coms.frame_locked = True
+            coms.ns = "/sl1m_ros/solution/coms"
+            ret.markers.append(coms)
+
+        moving_feet_pos = Marker()
+        if result.moving_feet_pos:
+            for pose in result.moving_feet_pos:
+                p = Point()
+                p.x = pose[0][0]
+                p.y = pose[0][1]
+                p.z = pose[0][2]
+                moving_feet_pos.points.append(p)
+            r, g, b = self.colors[len(result.all_feet_pos) + 2]
+            moving_feet_pos.color.r = r
+            moving_feet_pos.color.g = g
+            moving_feet_pos.color.b = b
+            moving_feet_pos.header = commun_header
+            moving_feet_pos.header.frame_id = self.all_polygons_frame_id
+            moving_feet_pos.type = Marker.SPHERE_LIST
+            moving_feet_pos.action = Marker.ADD
+            moving_feet_pos.pose.orientation.w = 1.0
+            moving_feet_pos.scale.x = 0.05
+            moving_feet_pos.scale.y = 0.05
+            moving_feet_pos.scale.z = 0.05
+            moving_feet_pos.color.a = 1.0
+            moving_feet_pos.frame_locked = True
+            moving_feet_pos.ns = "/sl1m_ros/solution/moving_feet_pos"
+            ret.markers.append(moving_feet_pos)
+
+        for i, (limb, end_eff_pose) in enumerate(zip(self.params.limbs, result.all_feet_pos)):
+            marker = Marker()
+            if result.all_feet_pos:
+                for pose in end_eff_pose:
+                    if pose is not None:
+                        p = Point()
+                        p.x = pose[0]
+                        p.y = pose[1]
+                        p.z = pose[2]
+                        marker.points.append(p)
+            r, g, b = self.colors[i]
+            marker.color.r = r
+            marker.color.g = g
+            marker.color.b = b
+            marker.header = commun_header
+            marker.header.frame_id = self.all_polygons_frame_id
+            marker.type = Marker.SPHERE_LIST
+            marker.action = Marker.ADD
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 0.05
+            marker.scale.y = 0.05
+            marker.scale.z = 0.05
+            marker.color.a = 1.0
+            marker.frame_locked = True
+            marker.ns = "/sl1m_ros/solution/" + limb + "_pos"
+            ret.markers.append(marker)
+
+        for i, polygon in enumerate(polygons):
+            marker = Marker()
+            marker.points.clear()
+            for j in range (polygon.shape[1]):
+                p = Point()
+                p.x = polygon[0, j]
+                p.y = polygon[1, j]
+                p.z = polygon[2, j]
+                marker.points.append(p)
+            marker.points.append(marker.points[0])
+            r, g, b = self.color(i, len(polygons))
+            marker.color.r = r
+            marker.color.g = g
+            marker.color.b = b
+            marker.header = Header()
+            marker.header.frame_id = "memmo_staircase"
+            marker.type = Marker.LINE_STRIP
+            marker.action = Marker.ADD
+            marker.id = i
+            marker.pose.position.x = 0.0
+            marker.pose.position.y = 0.0
+            marker.pose.position.z = 0.0
+            marker.pose.orientation.x = 0.0
+            marker.pose.orientation.y = 0.0
+            marker.pose.orientation.z = 0.0
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 0.05
+            marker.scale.y = 0.05
+            marker.scale.z = 0.05
+            marker.color.a = 1.0
+            marker.frame_locked = True
+            marker.ns = "/sl1m/polygon_used"
+            ret.markers.append(marker)
+        return ret
+
+    def add_start_polygon(self, all_polygons, initial_contacts, margin=0.2):
+        np_init_c = np.array(initial_contacts).T
+        x_min = np.min(np_init_c[0, :]) - margin
+        x_max = np.max(np_init_c[0, :]) + margin
+        y_min = np.min(np_init_c[1, :]) - margin
+        y_max = np.max(np_init_c[1, :]) + margin
+        z = np.average(np_init_c[2, :])
+        start_polygon = np.array(
+            [
+                [x_max, y_max, z],
+                [x_min, y_max, z],
+                [x_min, y_min, z],
+                [x_max, y_min, z],
+            ]
+        ).T
+        all_polygons.append(start_polygon)
+        return all_polygons
+
+    def color(self, i, i_max):
+        hue = float(i) / float(i_max)
+        print(i, i_max, hue)
+        (r, g, b) = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+        return int(255 * r), int(255 * g), int(255 * b)
--- src/sl1m_ros/sl1m_params.py
+++ src/sl1m_ros/sl1m_params.py
@@ -8,14 +8,14 @@ class Sl1mParameters:
     def __init__(self) -> None:
             
         # public params
-        self.solver_method = "MIP"
+        self.use_sl1m = "MIP"
         self.paths = []
         self.limbs = []
         self.suffix_com = ""
         self.suffix_feet = ""
         self.gait = []
         self.plot = False
-        self.cost = {}
+        self.costs = {}
         self.nb_steps = 0
         self.rate = 20
         self.optimize_com = True
@@ -36,7 +36,7 @@ class Sl1mParameters:
             return Solvers.CVXPY
 
     def get_ros_param(self):
-        self.solver_method = self._get_param("/sl1m/solver_method")
+        self.use_sl1m = self._get_param("/sl1m/use_sl1m")
         self._numerical_solver = self._get_param("/sl1m/numerical_solver")
         self.paths = self._get_param("sl1m/paths")
         self.limbs = self._get_param("sl1m/limbs")
@@ -44,7 +44,7 @@ class Sl1mParameters:
         self.suffix_feet = self._get_param("sl1m/suffix_feet")
         self.gait = self._get_param("sl1m/gait")
         self.plot = self._get_param("sl1m/plot")
-        self.cost = self._get_param("sl1m/cost")
+        self.costs = self._get_param("sl1m/costs")
         self.nb_steps = self._get_param("sl1m/nb_steps")
         self.rate = self._get_param("sl1m/rate")
         self.optimize_com = self._get_param("sl1m/optimize_com")
@@ -66,14 +66,13 @@ class Sl1mParameters:
             self.gait[i] = np.array(self.gait[i])
 
         # assert sanity checks on content
-        if self.cost:
+        if self.costs:
             pass
 
     def _get_param(self, name):
         if rospy.has_param(name):
             return rospy.get_param(name)
         else:
-            print(self.__dict__)
             var_name = str(Path(name).name)
             if var_name == "numerical_solver":
                 return self.__dict__["_" + var_name]
@@ -82,7 +81,7 @@ class Sl1mParameters:
 
     def __repr__(self):
         ret = "Sl1mParameters\n"
-        ret += "    - solver_method = " + str(self.solver_method) + "\n"
+        ret += "    - use_sl1m = " + str(self.use_sl1m) + "\n"
         ret += "    - numerical_solver = " + str(self._numerical_solver) + "\n"
         ret += "    - paths = " + str(self.paths) + "\n"
         ret += "    - limbs = " + str(self.limbs) + "\n"
@@ -90,7 +89,7 @@ class Sl1mParameters:
         ret += "    - suffix_feet = " + str(self.suffix_feet) + "\n"
         ret += "    - gait = " + str(self.gait) + "\n"
         ret += "    - plot = " + str(self.plot) + "\n"
-        ret += "    - cost = " + str(self.cost) + "\n"
+        ret += "    - costs = " + str(self.costs) + "\n"
         ret += "    - nb_steps = " + str(self.nb_steps) + "\n"
         ret += "    - rate = " + str(self.rate) + "\n"
         ret += "    - optimize_com = " + str(self.optimize_com) + "\n"
